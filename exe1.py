2fbcbf7b34478b07f0ce04000c129b124fc1b0c6
Golan456 <Golan44556@gmail.com>
2024-04-13 17:21:16 +0300

    Initial commit

---
+++ exe1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+print(arr1)
+arr2 = np.arange(10)  # generates a list of numbers within the range of the digit passed
+print(arr2)
+arr3 = np.zeros_like(arr1)
+print(arr3)
+arr4 = np.eye(5)
+print(arr4)
+print("hello world")
